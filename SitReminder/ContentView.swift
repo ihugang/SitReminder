bb1e1708bec03bdbafcbf47ac48a7fd29b5d9fa3
hugang <hugang@codans.com>
2025-05-06 11:43:00 +0800

    Initial Commit

---
+++ SitReminder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SitReminder
+//
+//  Created by Hu Gang on 2025/5/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SitReminder/SitReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SitReminderApp.swift
+//  SitReminder
+//
+//  Created by Hu Gang on 2025/5/6.
+//
+
+import SwiftUI
+
+@main
+struct SitReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
